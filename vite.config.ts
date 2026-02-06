2b9f18f547e8dd965fe33934e790b81143fc600d
Shaikh Ahmed Azad <105549990+ShaikhAhmedAzad@users.noreply.github.com>
2026-02-06 09:54:02 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/OpenCivil-Web/",  //
+  base: "/",  //
   server: {
     host: "::",
     port: 8080,
